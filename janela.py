7aa8f6ab5e37cacfa7e45c4e3218fafc23fce641
Lucas <lucas187_ro@hotmail.com>
2024-07-11 12:42:02 -0300

    Atualização automática dos arquivos CSV

--- janela.py
+++
@@ -1,9 +0,0 @@
-import pygetwindow as gw
-
-def encontrar_janela_excel():
-    for window in gw.getAllTitles():
-        if "Excel" in window:
-            print(window)
-
-# Chamar a função para encontrar a janela do Excel
-encontrar_janela_excel()

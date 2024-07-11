96a056912c7b1962148b409f9967f6d4314acff4
Lucas <lucas187_ro@hotmail.com>
2024-07-11 11:24:17 -0300

    Atualização automática dos arquivos CSV

--- Project_webScrp_amazon/scraper.py
+++ Project_webScrp_amazon/scraper.py
@@ -66,7 +66,8 @@ def push_to_github():
     repo_dir = os.getcwd()  # Diretório do repositório
     subprocess.run(["git", "add", "."], cwd=repo_dir)
     subprocess.run(["git", "commit", "-m", "Atualização automática dos arquivos CSV"], cwd=repo_dir)
-    subprocess.run(["git", "push", "--set-upstream", "origin", "main"], cwd=repo_dir)
+    subprocess.run(["git", "pull", "--rebase"], cwd=repo_dir)
+    subprocess.run(["git", "push"], cwd=repo_dir)
 
 # Função para converter CSV para JSON e salvar
 def convert_csv_to_json():
